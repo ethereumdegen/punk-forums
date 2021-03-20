9d8cdb5d9dd0469abae963c34cf759accf6b164a
Andy <admazzola@gmail.com>
2021-03-20 15:57:28 -0400

    cors

--- server/lib/packet-receiver.js
+++ server/lib/packet-receiver.js
@@ -38,14 +38,18 @@ export default class PacketReceiver  {
     {
     
     
-        //THIS IS CORRECT 
+        
       let options={
-        cors: {
-          origin: "*",
-          methods: ["GET", "POST"],
-          
+        handlePreflightRequest: (req, res) => {
+            const headers = {
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+                "Access-Control-Allow-Credentials": true
+            };
+            res.writeHead(200, headers);
+            res.end();
         }
-      }
+    }
      
     
       
