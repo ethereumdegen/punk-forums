480131123b75757225fc6e45628dae7f8fc53aa5
Andy <admazzola@gmail.com>
2021-03-20 15:59:19 -0400

    socket io v

--- server/lib/packet-receiver.js
+++ server/lib/packet-receiver.js
@@ -38,18 +38,14 @@ export default class PacketReceiver  {
     {
     
     
-        
+        //THIS IS CORRECT for v3 
       let options={
-        handlePreflightRequest: (req, res) => {
-            const headers = {
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-                "Access-Control-Allow-Credentials": true
-            };
-            res.writeHead(200, headers);
-            res.end();
+        cors: {
+          origin: "*",
+          methods: ["GET", "POST"],
+          
         }
-    }
+      }
      
     
       
