1252fe90a87ca5bdd2a3ee8631985372d6215a9e
Andy <admazzola@gmail.com>
2021-03-20 16:04:23 -0400

    cors

--- server/lib/packet-receiver.js
+++ server/lib/packet-receiver.js
@@ -20,13 +20,13 @@ export default class PacketReceiver  {
 
         var server = http.createServer(app);
  
-        app.use(cors());
+        //app.use(cors());
 
-         /*app.all('/*', function(req, res, next) {
+          app.all('/*', function(req, res, next) {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Content-Type,accept,access_token,X-Requested-With');
             next();
-        });*/
+        }); 
 
        
 
