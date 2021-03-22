93adcb5b3970b3deb6e24f2621d162495df9c58b
Andy <admazzola@gmail.com>
2021-03-22 19:35:32 -0400

    fix svg

--- src/views/components/GenericTable.vue
+++ src/views/components/GenericTable.vue
@@ -7,7 +7,7 @@
                
             </tr>
           </thead>
-          <tbody>
+          <tbody v-cloak>
             <tr v-for="row of rowsArray" class="hover:bg-teal-200 cursor-pointer" @click="clickedRow(row)">
                
               <td v-for="(label, index) in labelsArray" >{{Object.values(row)[index]}}</td>
@@ -17,7 +17,7 @@
           </tbody>
         </table>
 
-        <div v-if="!rowsArray || rowsArray.length == 0">
+        <div v-if="!rowsArray || rowsArray.length == 0" v-cloak>
           No records found.
         </div>
   </div>
