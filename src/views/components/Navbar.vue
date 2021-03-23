94938b6dd7b3f2103e77e567e64b51ad1d12c36b
Andy <admazzola@gmail.com>
2021-03-23 17:37:33 -0400

    logo

--- src/views/components/Navbar.vue
+++ src/views/components/Navbar.vue
@@ -3,7 +3,9 @@
     <div class=" bg-transparent">
     <div class="container w-container">
       <div class="w-full flex flex-row">
-
+        <div class="brand">
+        <img class="inline-block  " width="24" height="24" src="/logo.png" />
+        </div>
         <router-link to="/" class='brand w-nav-brand w--current flex-grow inline text-xl text-gray-900 font-bold'>BUY THE FLOOR</router-link>
 
  
