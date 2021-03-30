9b3a0c83b9222fa530095a56aad9cd62931c4165
andy <admazzola@gmail.com>
2021-03-30 11:18:55 -0400

    can sell to projectid and non projectid

--- src/views/components/NFTSellForm.vue
+++ src/views/components/NFTSellForm.vue
@@ -298,6 +298,7 @@ export default {
 
                                                             nftContractName: BuyTheFloorHelper.getNameFromContractAddress(pkt.nftContractAddress,pkt.projectId, chainId),
                                                             nftContractAddress: pkt.nftContractAddress,
+                                                            requireProjectId: pkt.requireProjectId,
                                                             projectId: pkt.projectId,
                                                             currencyTokenAddress: pkt.currencyTokenAddress,
                                                             currencyTokenAmount: pkt.currencyTokenAmount,
