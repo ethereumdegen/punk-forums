e79cd439d925b7d59c72656ba340be3c4b2cdae1
andy <admazzola@gmail.com>
2021-04-01 16:49:11 -0400

    commenst

--- server/dataghost.js
+++ server/dataghost.js
@@ -28,18 +28,7 @@ let dataghostConfig = dataghostConfigFile[envmode]
  
     console.log('web3 ready with provider ',serverConfig.web3provider )
 
- 
-   /* let wolfPackConfig = {
-        contracts:[{address:"0x39ec448b891c476e166b3c3242a90830db556661", startBlock: 4465713, type:'ERC721'},
-                        {address:"0x7cea7e61f8be415bee361799f19644b9889713cd", startBlock: 4528636, type:'ERC721'}],
-         
-        suffix:"dev",
-        indexRate: 10*1000,
-        courseBlockGap: 8000,
-        logging:true,
-        reScale: false
-    }*/
-
+  
     let wolfPackConfig = dataghostConfig.wolfPackConfig
 
 
@@ -48,39 +37,7 @@ let dataghostConfig = dataghostConfigFile[envmode]
     wolfPack.startIndexing( web3, wolfPackConfig )  
 
 
-
-
-    //need to grab deposit and mint events 
-
-
-    /*
-        let tfc = dataghostConfig.tinyfoxconfigs[index]
-
-        let delay = index * 25 * 1000;
-
-        let tinyfoxConfig = {
-            // contractType: 'ERC20',
-            // contractAddress: '0xab89a7742cb10e7bce98540fd05c7d731839cf9f' ,
-          //   startBlock: 1316824,
-              indexRate: 50 * 1000,
-
-             contractType: 'ERC20', 
-             courseBlockGap: 10000,
-             logging:true,
-             
-             suffix: tfc.suffix,
-             
-             
-             contractAddress: tfc.contractAddress,
-             startBlock: tfc.startBlock 
-             
-         } 
-
-        setTimeout(function(){ initTinyFox(web3, tinyfoxConfig) } , delay)
-        */
-     
-    
-
+  
      
 
 } 
