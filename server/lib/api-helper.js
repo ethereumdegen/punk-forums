086a9c257f17b66ecef5f3dde0b294008a5d89ec
andy <admazzola@gmail.com>
2021-04-09 15:25:08 -0400

    improved api

--- server/lib/api-helper.js
+++ server/lib/api-helper.js
@@ -28,9 +28,32 @@
 
                 let results = await APIHelper.findAllERC721ByOwner(publicAddress, mongoInterface)
 
+                return {success:true, input: inputParameters, output: results  }
+            }
+
+
+            if(inputData.requestType == 'burned_ERC20_by_from'){
+ 
+                let inputParameters = inputData.input
+
+                let from = inputParameters.from 
+
+                let results = await APIHelper.findBurnedERC20ByFrom(from, mongoInterface)
 
                 return {success:true, input: inputParameters, output: results  }
             }
+
+            if(inputData.requestType == 'burned_ERC20_by_token'){
+ 
+                let inputParameters = inputData.input
+
+                let token = inputParameters.token 
+
+                let results = await APIHelper.findBurnedERC20ByToken(token, mongoInterface)
+
+                return {success:true, input: inputParameters, output: results  }
+            }
+
 
             return {success:false}
         }
@@ -38,6 +61,17 @@
         static async findAllERC721ByOwner(publicAddress,mongoInterface){
             publicAddress = web3utils.toChecksumAddress(publicAddress)
             return await mongoInterface.findAll('erc721_balances',{accountAddress: publicAddress })
+        }
+
+
+        static async findBurnedERC20ByFrom(publicAddress,mongoInterface){
+            publicAddress = web3utils.toChecksumAddress(publicAddress)
+            return await mongoInterface.findAll('erc20_burned',{from: publicAddress })
+        }
+
+        static async findBurnedERC20ByToken(tokenAddress,mongoInterface){
+            tokenAddress = web3utils.toChecksumAddress(tokenAddress)
+            return await mongoInterface.findAll('erc20_burned',{token: tokenAddress })
         }
     
          
